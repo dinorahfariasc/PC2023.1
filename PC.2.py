8f624fb9f6c38cae6c25fdb6ef5583c4dee95b16
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2023-06-22 10:47:05 -0300

    Update PC.2.py

--- PC.2.py
+++ PC.2.py
@@ -26,3 +26,41 @@ elif esc == 2:
         if num % x == 0:
             count += 1
     print(f"o numero {num} é primo") if count == 2 else print(f"o numero {num} é composto")
+    
+#=========================================================================================
+
+# Márveu versus Cápicon
+from math import floor, ceil
+
+entradas = []
+for x in range(5):
+    entradas.append(int(input("digite um número: ")))
+
+# min e maximo
+print(f"Marveu: menor entrada = {min(entradas)}")
+print(f"capicon: maior entrada ={ max(entradas)}\n")
+
+print("index minimo e maximo")
+print(f"Marveu: minimo foi o {entradas.index(min(entradas)) +1}° numero informado")
+print(f"capicon: maximo foi o {entradas.index(max(entradas))+1}° numero informado\n")
+
+print("soma e produto")
+print(f"Marveu: soma das entrada = {sum(entradas)}")
+produto = 1
+for x in entradas:
+    produto *= x
+print(f"capicon: produto das entradas = {produto}\n")
+
+print("media e mediana")
+print(f"Marveu: menor entrada = {sum(entradas)/(len(entradas)+1)}")
+mediana = 0
+if len(entradas) % 2 == 0:
+    ind = int((len(entradas)+1)/2)
+    mediana = round(entradas[ind] + entradas[ind+1]/2)
+else:
+    ind = int(len(entradas)/2)
+    mediana = entradas[ind]
+
+print(f"capicon: maior entrada ={mediana}\n")
+
+print(entradas)
