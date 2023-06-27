308437330b44686b0fcb9923321244762ab0fc1c
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2023-06-27 16:04:12 -0300

    Add files via upload

--- PC3.py
+++ PC3.py
@@ -20,6 +20,31 @@
 
 # ================================================
 # testando combinacoes
+# def valordasenha(soma_valor, n):
+#     pares = []
+#
+#     for i in range(len(n)):
+#         for x in range(i + 1, len(n)):
+#             if n[i] + n[x] == soma_valor:
+#                 par = (n[i], n[x])
+#                 pares.append(par)
+#
+#     if len(pares) == 0:
+#         return None
+#
+#     pares.sort()
+#     senha = f"{pares[0][0]}{pares[0][1]}"
+#     print(senha)
+#
+# soma_valor = int(input("Insira a soma: "))
+# n = []
+# for i in range(10):
+#     numero = int(input(f"Digite o {i+1}º número: "))
+#     n.append(numero)
+#
+# senha = valordasenha(soma_valor, n)
+#
+# print("Sua Senha é:", senha)
 
 
 # ==========================================================
@@ -40,6 +65,6 @@
 # if len(teste) > len(maior):
 #     maior = teste
 #
-# 
+#
 # print(maior)
 #
